12aa61c1f9df9e429278bc3e18a8498d09434e31
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-12-08 11:32:19 +0530

    Ability to cretae binarysearch and print its size

--- JUnitBinarySearch/MyBinaryTreeTestCases.java
+++ JUnitBinarySearch/MyBinaryTreeTestCases.java
@@ -4,15 +4,38 @@ import org.junit.Test;
 
 import junit.framework.Assert;
 
-public class MyBinaryTreeTestCases {	
+public class MyBinaryTreeTestCases {
 	@Test
 	public void given3NumbersWhenAddedToBSTShouldReturnSize3() {
 		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
 		myBinaryTree.add(56);
 		myBinaryTree.add(30);
 		myBinaryTree.add(70);
-				
+
 		int size = myBinaryTree.getSize();
 		Assert.assertEquals(3, size);
+	}
+
+	@Test
+	public void given13NumbersWhenAddedToBSTShouldReturnSize13() {
+
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(11);
+		myBinaryTree.add(65);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
+
+		int size = myBinaryTree.getSize();
+		System.out.println("Test Case 2 - Size : "+size);
+		Assert.assertEquals(13, size);
 	}
 }
